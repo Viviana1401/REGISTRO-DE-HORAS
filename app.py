e2f16e950b48cd87a845f764ef70b970fc5dc311
Viviana1401 <90924175+Viviana1401@users.noreply.github.com>
2025-08-12 11:48:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,17 +15,9 @@ from datetime import datetime
 scope = ["https://spreadsheets.google.com/feeds", 
          "https://www.googleapis.com/auth/drive"]
 
-import os
-import json
-from oauth2client.service_account import ServiceAccountCredentials
-
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-
-credentials_info = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info, scope)
-
+# Usar el archivo credentials.json
+credentials = ServiceAccountCredentials.from_json_keyfile_name("contabilizador-de-horas-428f71314644.json", scope)
 client = gspread.authorize(credentials)
-
 
 # Abre la hoja de cálculo
 sheet = client.open("registro_horas").sheet1
@@ -54,6 +46,5 @@ if not data.empty:
     st.dataframe(data)
 else:
     st.info("No hay registros todavía.")
-
 
 
